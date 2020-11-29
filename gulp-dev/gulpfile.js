fd22751ebf02a476efe8130096b6fd49a9345af2
Vishinsky-Oleg <vishinsky.oleg@mail.com>
2020-11-30 02:47:41 +0300

    Gulpfile changes

--- gulp-dev/gulpfile.js
+++ gulp-dev/gulpfile.js
@@ -10,32 +10,32 @@ const gulp = require("gulp"),
     lineec = require("gulp-line-ending-corrector");
 
 const root = "../"; //Root folder
-const scss = root + "src/sass/", //Folder with scss files
+const scss = root + "src/scss/", //Folder with scss files
     js = root + "src/js/", //folder with js files
     vendor = js + "vendor/", //JS vendors
     jsdist = root + "dist/js/", //folder to place minified version of js
     cssdist = root + "dist/css/"; //folder for final css file
 
-const twigWatchFiles = root + "**/*.twig", //Files that is gonna be changed
-    styleWatchFiles = scss + "**/*.sass"; //if scss is changed
+const pagesWatchFiles = root + "**/*.html", //Files that is gonna be changed
+    styleWatchFiles = scss + "**/*.scss"; //if scss is changed
 
 const jsSRC = [
     //order in which js files will be processed
     js + "main.js",
-    vendor + "jquery-2.1.1.min.js",
-    vendor + "bootstrap.js",
-    vendor + "jquery.magnific-popup.min.js",
+    // vendor + "jquery-2.1.1.min.js",
+    // vendor + "bootstrap.js",
+    // vendor + "jquery.magnific-popup.min.js",
 ];
 
 const cssSRC = [
     //order in which css files will be processed
-    root + "src/css/vendor/bootstrap.css", //css vendors
+    // root + "src/css/vendor/bootstrap.css", //css vendors
     root + "src/css/style.css", //compiled sass
 ];
 
 function css() {
     return gulp
-        .src([scss + "style.sass"]) //sass file to compile
+        .src([scss + "style.scss"]) //sass file to compile
         .pipe(sourcemaps.init({ loadMaps: true }))
         .pipe(
             sass({
@@ -76,7 +76,7 @@ function watch() {
     });
     gulp.watch(styleWatchFiles, gulp.series([css, concatCSS]));
     gulp.watch(jsSRC, javascript);
-    gulp.watch([jsdist + "main.js", cssdist + "style.min.css"]).on(
+    gulp.watch([jsdist + "main.js", cssdist + "style.min.css", pagesWatchFiles]).on(
         "change",
         browserSync.reload
     );
